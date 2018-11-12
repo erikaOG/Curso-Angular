15bbf68470cb3177e3046581757b1ad8ab3aa2f8
erikaOG <ericckaa95@gmail.com>
2018-11-11 22:17:58 -0500

    curso angular

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export class Hero {
+    id: number;
+    nombre: string;
+    apellido: string;
+    
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] =[
+    {id: 1, nombre: 'Veronica', apellido: 'Benitez'},
+    {id: 2, nombre: 'Mercedes', apellido: 'Moscoso'},
+    {id: 3, nombre: 'Monica', apellido: 'Guerra'},
+    {id: 4, nombre: 'Carlos', apellido:'Perez'}
+    
+
+
+]
